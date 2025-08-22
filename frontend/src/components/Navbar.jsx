195862dba01a4d6db24708e1982538982b85ab85
Dagimawi7 <20dtamrat@gmail.com>
2025-08-22 14:09:24 -0400

    completed the functionlaiy of navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,46 @@
 import React from 'react'
 import {assets} from '../assets/assets.js'
+// import images like logo from assets file
 import { NavLink } from 'react-router-dom'
+// NavLink lets us create clickable links that change the page without reload
 
 const Navbar = () => {
   return (
     // a flexbox container that spaces items evenly
+    // main navbar container
+    // "flex" → use flexbox
+    // "items-center" → align items vertically in the center
+    // "justify-between" → put logo on left, menu on right
+    // "py-5" → padding top & bottom
+    // "font-medium" → medium text weight
     <div className='flex items-center justify-between py-5 font-medium'>
 
         <img src={assets.ztlogo} className='w-36' alt="brand logo" />
-
+        {/* Navigation links (menu) */}
+        {/* hidden on very small screens, visible on sm+ screens */}
+        {/* gap-5 → space between items */}
+        {/* text-sm → small text */}
+        {/* text-gray-700 → gray colored text */}
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-          <NavLink className={'flex flex-col items-center gap-1'}>
-            <p>Home</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
-  
-
+          <NavLink to='/' className={'flex flex-col items-center gap-1'}>
+            <p>HOME</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+          </NavLink>
+          <NavLink to='/collection' className={'flex flex-col items-center gap-1'}>
+            <p>COLLECTION</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+          </NavLink>
+          <NavLink to='/about' className={'flex flex-col items-center gap-1'}>
+            <p>ABOUT</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+          </NavLink>
+          <NavLink to='/contact' className={'flex flex-col items-center gap-1'}>
+            <p>CONTACT</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
           </NavLink>
         </ul>
+
+        
     </div>
   )
 }
