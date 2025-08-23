c296261a2e006b26a07a54518fabf9a1d9045375
Dagimawi7 <20dtamrat@gmail.com>
2025-08-23 00:53:45 -0400

    Prepared LatestCollection component for displaying products

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const LatestCollection = () => {
+    // Get the products data from ShopContext
+    const { products } = useContext(ShopContext);
+     // Just to check: print the products in the console
+    console.log(products)
+
   return (
     <div>
-      
+      {/* Products will be displayed here later */}
     </div>
   )
 }
