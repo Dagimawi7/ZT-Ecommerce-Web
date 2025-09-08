46d5441d05a325dc5996ed7a9cd36e1ce11940a1
Dagimawi7 <20dtamrat@gmail.com>
2025-09-07 21:01:46 -0400

    add filters and sorting to collection page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
-import Collection from './pages/Collection'
+import Collection from './pages/Collection';
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
@@ -34,6 +34,7 @@ const App = () => {
         <Route path='/orders' element={<Orders/>}/>
       </Routes>
       <Footer/>
+
     </div>
   )
 }
