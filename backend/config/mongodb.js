0ca56723b088e7489a8b4939d40503252d0c96e6
Dagimawi7 <20dtamrat@gmail.com>
2025-09-20 21:11:29 -0400

    connect backend to Cloudinary for image uploads

---
+++ backend/config/mongodb.js
@@ -0,0 +1,20 @@
+// Import mongoose library to connect to MongoDB
+import mongoose from "mongoose"
+
+// Function to connect to MongoDB
+const connectDB = async () => {
+
+    // Event listener: when mongoose successfully connects, log message
+    mongoose.connection.on('connected', () => {
+        console.log("DB Connected") // Shows database connection is successful
+    })
+
+    // Connect to MongoDB using the URI from .env file and 'e-commerce' database
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+}
+
+// Export the connectDB function to use it in other files
+export default connectDB
+
+
+// onnects the backend to a MongoDB database and confirms when the connection is successful.
