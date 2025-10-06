4d13b70acc3a61ede54698e8c43d5ddec7ab0887
Dagimawi7 <20dtamrat@gmail.com>
2025-10-05 20:17:29 -0400

    added addProduct function to save product info and upload images to cloudinary

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,16 +1,83 @@
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel.js";
+
 // Function to add a new product to the database
 const addProduct = async (req, res) => {
-  
+  try {
+    // Get product details from the request body
+    const {
+      name,
+      description,
+      price,
+      category,
+      subcategory,
+      sizes,
+      bestseller,
+    } = req.body;
+
+    // Get uploaded images from the request files (if they exist)
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    // Put all images into an array and remove any that are undefined
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+
+    // Upload all images to Cloudinary and get their secure URLs
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url; // Return the secure URL of each uploaded image
+      })
+    );
+
+    // Prepare the product data object to save in the database
+    const productData = {
+      name,
+      description,
+      category,
+      price: Number(price), // Convert price to number
+      subcategory,
+      bestseller: bestseller === "true" ? true : false, // Convert string to boolean
+      sizes: JSON.parse(sizes), // Convert sizes from string to array
+      image: imagesUrl, // Add uploaded image URLs
+      date: Date.now(), // Save the current date
+    };
+
+    console.log(productData); // Print product data for debugging
+
+    // Create a new product instance and save it to the database
+    const product = new productModel(productData);
+    await product.save();
+
+    // Send success response
+    res.json({ success: true, message: "Product Added" });
+  } catch (error) {
+    // If an error occurs, log it and send an error response
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 // Function to get (list) all products from the database
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+
+};
 
 // Function to delete a product from the database
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+
+};
 
 // Function to get details of a single product
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  
+};
 
 // Exporting all functions so they can be used in other files
 export { listProducts, addProduct, removeProduct, singleProduct };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,14 +5,23 @@ import {
   addProduct,
   removeProduct,
   singleProduct,
-} from "../controllers/productController";
-import upload from "../middleware/multer";
+} from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
 
 // Creating a new router for handling product-related routes
 const productRouter = express.Router();
 
 // Route to add a new product with up to 4 uploaded images
-productRouter.post("/add",upload.fields([{name:'image1', maxCount:1},{name:'image2', maxCount:1}, {name:'image3', maxCount:1},{name:'image4', maxCount:1} ]),addProduct);
+productRouter.post(
+  "/add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
 
 // Route to remove a product (POST request)
 productRouter.post("/remove", removeProduct);
@@ -23,5 +32,5 @@ productRouter.post("/single", singleProduct);
 // Route to list all products (GET request)
 productRouter.get("/list", listProducts);
 
-// Exporting the router so it can be used in other files 
+// Exporting the router so it can be used in other files
 export default productRouter;
